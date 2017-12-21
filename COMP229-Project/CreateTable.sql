d57b0cd499e16174cb56a80e81a61cb89a82b587
Jupiter1997 <jupiter.jv@gmail.com>
2017-12-21 12:36:42 -0500

    Added SQL Files

---
+++ COMP229-Project/CreateTable.sql
@@ -0,0 +1,15 @@
+USE [ShalomLibrary]
+Go
+Create Table [Book](
+[BookID][int] NOT NULL IDENTITY (1,1),
+[Title][nvarchar](50) ,
+[Author][nvarchar](50),
+[ISBN][int],
+[Genre][nvarchar](50),
+[NoOfPages][nvarchar](10),
+[LentToFriend][nvarchar](50),
+[FriendName][nvarchar](50),
+[Comments][nvarchar](50)
+)
+
+
