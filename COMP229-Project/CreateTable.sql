a0814d3b18506c97a72f3a96abca78aecda25fb1
Mr. Marvin Jupiter Roilo Vargas <mvargas2@my.centennialcollege.ca>
2017-12-22 00:00:10 -0500

    Safe

--- COMP229-Project/CreateTable.sql
+++ COMP229-Project/CreateTable.sql
@@ -4,12 +4,13 @@ Create Table [Book](
 [BookID][int] NOT NULL IDENTITY (1,1),
 [Title][nvarchar](50) ,
 [Author][nvarchar](50),
-[ISBN][int],
+[ISBN][nvarchar](20),
 [Genre][nvarchar](50),
 [NoOfPages][nvarchar](10),
 [LentToFriend][nvarchar](50),
 [FriendName][nvarchar](50),
 [Comments][nvarchar](50)
 )
+
 
 
